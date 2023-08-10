85c0d6d582293270f35357176266cf21aeb4c714
unknown <khawki00@hotmail.com>
2023-08-10 11:34:13 -0400

    Initial Commit

---
+++ src/main/java/online/krashlearning/bookstore/dto/BookDto.java
@@ -0,0 +1,18 @@
+package online.krashlearning.bookstore.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class BookDto {
+    private String title;
+
+
+
+}
